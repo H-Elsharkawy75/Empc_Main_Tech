9c7cccc97a75c7130ebd3064b3f5f032c8587375
amr nasser <amrelzanaty0@gmail.com>
2022-06-17 08:12:30 +0200

    Iframe

---
+++ Eagles.LMS/Migrations/202206152358062_iframeslide.cs
@@ -0,0 +1,18 @@
+﻿namespace Eagles.LMS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class iframeslide : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Sliders", "ArabicIframe", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Sliders", "ArabicIframe");
+        }
+    }
+}
